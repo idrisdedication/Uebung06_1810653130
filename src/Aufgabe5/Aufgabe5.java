3d7636615d577d28dd8b705355aa60ed4bfb293f
unknown <you@example.com>
2019-01-08 20:42:22 +0100

    Aufgabe 5

--- src/Aufgabe5/Aufgabe5.java
+++ src/Aufgabe5/Aufgabe5.java
@@ -11,9 +11,9 @@ public class Aufgabe5 {
 
         studentList.add(new Student("Abdullatif", 1810653130, 2018));
 
-        studentList.add(new Student("Idris Ihvan Inayetullah", 0010653130, 2017));
+        studentList.add(new Student("Idris Ihvan Inayetullah", 1010653130, 2017));
 
-        studentList.add(new Student("Mahmut Efendi Hazretleri", 0110653130, 2016));
+        studentList.add(new Student("Mahmut Efendi Hazretleri", 1110653130, 2016));
 
         for (Student s: studentList){
 
